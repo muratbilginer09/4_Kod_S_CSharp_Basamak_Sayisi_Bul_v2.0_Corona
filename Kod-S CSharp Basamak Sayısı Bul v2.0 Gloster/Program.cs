86240110343729d66ef07b3c512ff074321a8b69
Murat Bilginer <mrtblgnr@muratbilginer.net>
2016-04-23 00:29:05 +0300

    4 Kod-S CSharp Proje Basamak Sayısı Bul v2.0 Corona
    Girilen değerin basamak sayısını bulan programın 2. versiyonu.
    Geliştirilmiş algoritma.
    Azaltılmış kod satırı.

---
+++ Kod-S CSharp Basamak Sayısı Bul v2.0 Gloster/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kod_S_CSharp_Basamak_Sayısı_Bul_v2._0_Gloster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sayi;
+
+            Console.Write("Sayıyı Giriniz:");
+
+            sayi = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Basamak Sayısı={0}",sayi.Length);
+
+            Console.ReadKey();
+
+        }
+    }
+}
